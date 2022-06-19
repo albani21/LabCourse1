6c7e2b62f2e1767c1804f2ad32c053bbe15d395f
granitlekaj <73529239+granitlekaj@users.noreply.github.com>
2022-06-19 17:02:42 +0200

    Add files via upload

---
+++ Vizitori.cs
@@ -0,0 +1,13 @@
+﻿namespace PrisonBackEndLab1
+{
+    public class Vizitori
+    {
+        public int VizitoriID { get; set; }
+        public string? Emri { get; set; }
+        public string? Mbiemri { get; set; }
+
+        public int ButgosuriID { get; set; }
+        public string? EmriIBurgosurit { get; set; }
+
+    }
+}
