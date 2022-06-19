fb85870039fa91d9c061ae24b29ae0e30b816f77
granitlekaj <73529239+granitlekaj@users.noreply.github.com>
2022-06-19 17:16:25 +0200

    Add files via upload

---
+++ Vizita.cs
@@ -0,0 +1,15 @@
+﻿namespace PrisonBackEndLab1
+{
+    public class Vizita
+    {
+        public int VizitaID { get; set; }
+
+        public int BurgosuriID{ get; set; }
+        public int VizitoriID { get; set; }
+
+        public DateTime? Data { get; set; }
+
+        public DateTime? KohaFillimit { get; set; }
+        public DateTime? KohaMbarimit { get; set; }
+    }
+}
